49793f72ad15398e8d4a863f39af164a5f64120e
Levi Powis <lpowis97@gmail.com>
2023-12-03 13:27:33 -0500

    adds import for bootstrap to main.jsx - adds LogoutLink.jsx - updates Header.jsx with navbar bootstrap styling and authlinks showing based if logged in or not

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,63 @@
 import { Link } from "react-router-dom";
+import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
+  let authLinks;
+
+  if (localStorage.jwt === undefined) {
+    authLinks = (
+      <>
+        <li className="nav-item">
+          <Link className="nav-link" aria-current="page" to="/signup">
+            Signup
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" aria-current="page" to="/login">
+            Login
+          </Link>
+        </li>
+      </>
+    );
+  } else {
+    authLinks = (
+      <li className="nav-item">
+        <LogoutLink />
+      </li>
+    );
+  }
+
   return (
     <header>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  All Posts
+                </Link>
+              </li>
+              {authLinks}
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
